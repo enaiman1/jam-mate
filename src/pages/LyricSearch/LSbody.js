a8095b4cd327326cb081e7eba51217c9fda74908
Ericcentrik <enaiman1@gmail.com>
2020-07-05 18:08:39 -0400

    added lyric search components

---
+++ src/pages/LyricSearch/LSbody.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tracks from "../tracks/Tracks";
+import Search from "../tracks/Search";
+
+const LSbody = () => {
+  return (
+    <>
+      <Search />
+      <Tracks />
+    </>
+  );
+};
+
+export default LSbody;
