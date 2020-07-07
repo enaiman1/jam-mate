1b8bb3a994d1f21f369f9044fa8a56976ea91fcb
Ericcentrik <enaiman1@gmail.com>
2020-07-07 15:40:43 -0400

    add style and refactor of lyrics component

--- src/pages/LyricSearch/Lyrics/Lyrics.js
+++ src/pages/LyricSearch/Lyrics/Lyrics.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Button from "../../../components/Button/Button";
 import Spinner from "../Spinner/Spinner";
 import "./Lyrics.scss";
 
@@ -14,13 +15,13 @@ class Lyrics extends Component {
   componentDidMount() {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+        `http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then((res) => {
         this.setState({ lyrics: res.data.message.body.lyrics });
 
         return axios.get(
-          `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
+          `http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_MM_KEY}`
         );
       })
       .then((res) => {
@@ -43,35 +44,40 @@ class Lyrics extends Component {
     } else {
       return (
         <>
-          <Link to="/LSindex" className="btn btn-dark btn-sm mb-4">
-            Lyric Search
-          </Link>
-          <div className="card">
-            <h5 className="card-header">
+        <div className="lyrics">
+          <div className="lyrics__card">
+            <h5 className="lyrics__header">
               {track.track_name} by{" "}
-              <span className="text-secondary">{track.artist_name}</span>
+              <span className="lyrics__header__secondary">{track.artist_name}</span>
             </h5>
-            <div className="card-body">
+            <div className="lyrics__body">
               <p className="card-text">{lyrics.lyrics_body}</p>
             </div>
           </div>
 
           <ul className="list_group mt-3">
-            <li className="list-group-item">
+            <li className="lyrics__list">
               <strong>Album ID</strong>: {track.album_id}
             </li>
-            <li className="list-group-item">
+            <li className="lyrics__list">
               <strong>Song Genre</strong>:{" "}
               {track.primary_genres.music_genre_list.length === 0
                 ? "NO GENRE AVAILABLE"
                 : track.primary_genres.music_genre_list[0].music_genre
                     .music_genre_name}
             </li>
-            <li className="list-group-item">
+            <li className="lyrics__list">
               <strong>Explicit Words</strong>{" "}
               {track.explicit === 0 ? "No" : "Yes"}
             </li>
           </ul>
+          <Link to="/LSindex" className="button__box">
+            <Button className="Ltrack__action">
+              Back to Search
+            </Button>
+           
+          </Link>
+          </div>
         </>
       );
     }
