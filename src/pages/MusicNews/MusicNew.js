88038cbbe6cac8d8b94e63c798d429f0ed1e31f2
Ericcentrik <enaiman1@gmail.com>
2020-07-07 21:40:36 -0400

    added twitter-embed, work on updatez component

--- src/pages/MusicNews/MusicNew.js
+++ src/pages/MusicNews/MusicNew.js
@@ -1,17 +1,54 @@
 import React from "react";
-import "./MusicNew.scss"
+import { TwitterTimelineEmbed} from 'react-twitter-embed';
+import "./MusicNew.scss";
 
 const MusicNews = () => {
-    return (
-      <div className="musicNews">
-        <h1>
-        <span role="img" aria-label="Face With Rolling Eyes Emoji">
-          <p>This is where the music news will go</p>
-        </span>
-      </h1>
-      </div>
-        
-    )
-}
+  return (
+    <div className="musicNews">
+        <h1 className="musicNews__title">
+         Top Twitter Accounts for music news
+        </h1>
+      <div className="musicNews__body">
+        <div className="row">
+          {/* Rolling stone twitter account */}
+          <div className="col-lg-3 col-md-6 col-sm-12">
+                <TwitterTimelineEmbed
+                  sourceType="profile"
+                  screenName="RollingStone"
+                  options={{ width: 400, height: 600 }}
+                />
+          </div>
+          {/* The Hype machine twitter account */}
+          <div className="col-lg-3 col-md-6 col-sm-12">
+                <TwitterTimelineEmbed
+                  sourceType="profile"
+                  screenName="hypem"
+                  options={{ width: 400, height: 600 }}
+                />
+          </div>
+          {/* The billboard twitter account */}
+          <div className="col-lg-3 col-md-6 col-sm-12">
+                <TwitterTimelineEmbed
+                  sourceType="profile"
+                  screenName="billboard"
+                  options={{ width: 400, height: 600 }}
+                />
+          </div>
+          {/* music new & rumors twitter account */}
+          <div className="col-lg-3 col-md-6 col-sm-12">
+                <TwitterTimelineEmbed
+                  sourceType="profile"
+                  screenName="MusicNewsRumor"
+                  options={{ width: 400, height: 600 }}
+                />
+          </div>
+        </div>
+        <div className="row">
 
-export default MusicNews
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MusicNews;
