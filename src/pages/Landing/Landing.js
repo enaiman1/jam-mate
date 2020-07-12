e491283492a95f6a866735d81fd2c1ee2388badc
Ericcentrik <enaiman1@gmail.com>
2020-07-12 15:41:09 -0400

    added poster attribute to show image in landing page when in mobile

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,15 +1,19 @@
 import React from "react";
 import classes from "./Landing.module.scss";
 import VideoSource from "../../assets/video/dj.mp4";
+import VideoSourceM from "../../assets/video/dj.webm";
+import Poster from "../../assets/img/poster.jpg";
 import "./Landing.module.scss";
 
 
 const Landing = () =>{
     return (
         <header className={classes.header}>
-      <video playsInline autoPlay="autoplay" loop="loop" muted className={classes.Video}>
+
+      <video playsInline="playsinline" autoPlay="autoplay" loop="loop" autoBuffer="autobuffer" poster={Poster} className={classes.Video}>
         <source src={VideoSource} type="video/mp4" />
         <source src={VideoSource} type="video/ogg" />
+        <source src={VideoSourceM} type="video/webm"/>  
         Your browser does not support the video tag.
       </video>
       <div className={classes.header__textBox}>  
